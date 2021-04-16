d590d25785918273dd4f00b31b7f5702d28600ec
Andrew Palczewski <apj@apharmony.com>
2021-04-15 21:26:48 -0500

    JSHCMS-311 Use local cert in app.config.local.js

--- init/install.app.config.local.js
+++ init/install.app.config.local.js
@@ -1,7 +1,10 @@
 exports = module.exports = function(appConfig, installerParams, callback){
-  require('jsharmony-factory/init/install.app.config.local.js')(appConfig, installerParams, function(err){
+  if(!appConfig.params.jsharmony) appConfig.params.jsharmony = {};
+  appConfig.params.jsharmony.https_cert = "path.dirname(module.filename) + '/cert/localhost.crt'";
+  appConfig.params.jsharmony.https_key = "path.dirname(module.filename) + '/cert/localhost.key'";
+  require('jsharmony-cms/init/install.app.config.local.js')(appConfig, installerParams, function(err){
     if(err) return callback(err);
-
+    
     return callback();
   });
 }
